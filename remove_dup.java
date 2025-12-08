3f1ec0a963af2b992a31a718e100005099ad82ac
S-Afreed <123344810+S-Afreed@users.noreply.github.com>
2025-12-08 14:56:39 +0530

    Add files via upload
    Adding day 4 - program 2 [remove_duplicates and Sort]

---
+++ remove_dup.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+class UserMainCode {
+    public static String[] removeDuplicates(String[] arr) {
+        TreeSet<String> set = new TreeSet<>(); // Natural sorting, Uppercase first
+
+        for (String s : arr) {
+            set.add(s);  // Remove duplicates case-sensitively
+        }
+
+       String[] res = new String[set.size()];
+       int i=0;
+       for(String s:set){
+       	 res[i++]=s;
+       }
+       return res;
+    }
+}
+
+public class remove_dup {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        String[] result = UserMainCode.removeDuplicates(arr);
+
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+}
